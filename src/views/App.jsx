c97cc180df36ed48edaeef0d4a6041338f6eb5fe
Wendell Alves <wendellmccaulay@hotmail.com>
2023-03-19 12:52:36 -0300

    UseContext #01

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,20 +1,23 @@
-import './App.css'
-import React from 'react'
-import { BrowserRouter as Router } from  'react-router-dom'
+import "./App.css";
+import React, { useState } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 
-import Menu from '../components/layout/Menu'
-import Content from '../components/layout/Content'
+import Menu from "../components/layout/Menu";
+import Content from "../components/layout/Content";
+import DataContext, { data } from '../views/data/DataContext';
 
-const App = props => {
+const App = (props) => {
+  const [state, setState] = useState(data);  
+  return (
+    <DataContext.Provider value={{state, setState}}>
+      <div className="App">
+        <Router>
+          <Menu />
+          <Content />
+        </Router>
+      </div>
+    </DataContext.Provider>
+  );
+};
 
-    return (
-                <div className="App">
-                    <Router>
-                        <Menu />
-                        <Content />
-                    </Router>
-                </div>
-    )
-}
-
-export default App
+export default App;
