36baabd1ac908ceaab3dcb042a6d4c407a683508
Wendell Alves <wendellmccaulay@hotmail.com>
2023-03-19 15:37:22 -0300

    UseContext #02

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -4,19 +4,22 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 import Menu from "../components/layout/Menu";
 import Content from "../components/layout/Content";
-import DataContext, { data } from '../views/data/DataContext';
+import DataContext, { data } from "../views/data/DataContext";
+import Store from "../views/data/Store";
 
 const App = (props) => {
-  const [state, setState] = useState(data);  
+  const [state, setState] = useState(data);
   return (
-    <DataContext.Provider value={{state, setState}}>
-      <div className="App">
-        <Router>
-          <Menu />
-          <Content />
-        </Router>
-      </div>
-    </DataContext.Provider>
+    <Store>
+      <DataContext.Provider value={{ state, setState }}>
+        <div className="App">
+          <Router>
+            <Menu />
+            <Content />
+          </Router>
+        </div>
+      </DataContext.Provider>
+    </Store>
   );
 };
 
--- src/views/examples/UseContext.jsx
+++ src/views/examples/UseContext.jsx
@@ -1,16 +1,26 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import PageTitle from '../../components/layout/PageTitle';
+import SectionTitle from '../../components/layout/SectionTitle';
 import DataContext from '../data/DataContext';
+import { AppContext } from '../data/Store';
 
 const UseContext = (props) => {
-    const context = useContext(DataContext);
+    const {state, setState} = useContext(DataContext);
+    const { number, text, setNumber, setText } = useContext(AppContext);
 
-    function setNumber(delta) {
-        context.setState({ 
-            ...context.state,
-            number: context.state.number + delta 
+    function addNumber(delta) {
+        setState({ 
+            ...state,
+            number: state.number + delta 
         });
-    }
+    } 
+
+    useEffect(() => {
+        if (number > 1250) {
+            setText('Eita!!!');
+        }
+    }, [number]);
+
 
     return (
         <div className="UseContext">
@@ -18,13 +28,24 @@ const UseContext = (props) => {
                 title="Hook UseContext"
                 subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!"
             />
+            <SectionTitle title="Exercício #01" />
             <div className="center">
-                <span className="text">{ context.state.text }</span>
-                <span className="text">{ context.state.number }</span>
+                <span className="text">{ state.text }</span>
+                <span className="text">{ state.number }</span>
             </div>
             <div>
-                <button className="btn" onClick={() => setNumber(-1)}>-1</button>
-                <button className="btn" onClick={() => setNumber(1)}>+1</button>
+                <button className="btn" onClick={() => addNumber(-1)}>-1</button>
+                <button className="btn" onClick={() => addNumber(1)}>+1</button>
+            </div>
+
+            <SectionTitle title="Exercício #02" />
+            <div className="center">
+                <span className="text">{ text }</span>
+                <span className="text">{ number }</span>
+                <div>
+                    <button onClick={() => setNumber(number - 1)}>-1</button>
+                    <button onClick={() => setNumber(number + 1)}>+1</button>
+                </div>
             </div>
         </div>
     );
